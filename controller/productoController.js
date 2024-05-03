81a1b038d129abe3459b9f4f57afda55ce5a915b
Ezequielp19 <85073571+Ezequielp19@users.noreply.github.com>
2024-05-03 01:15:31 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ controller/productoController.js
@@ -0,0 +1,118 @@
+// controllers/productoController.js
+const Producto = require('../models/producto');
+const Categoria = require('../models/categoria');
+
+// Controlador para obtener todos los productos
+exports.getAllProductos = async (req, res) => {
+  try {
+    const productos = await Producto.find();
+    res.json(productos);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+
+
+// Controlador para crear un nuevo producto
+exports.createProducto = async (req, res) => {
+  try {
+    // Obtiene las categorías seleccionadas del cuerpo de la solicitud
+    const categoriasSeleccionadas = req.body.categorias;
+
+   // Agrega el console.log para imprimir req.body
+    console.log('Datos del cuerpo de la solicitud:', req.body);
+
+    // Crea un nuevo producto con los datos proporcionados
+    const producto = new Producto({
+      nombre: req.body.nombre,
+      descripcion: req.body.descripcion,
+      imagen: req.file.filename,
+      precio: req.body.precio,
+      descuento: req.body.descuento || 0,
+      precioFinal: req.body.precio - (req.body.descuento || 0),
+      categorias: categoriasSeleccionadas 
+    });
+
+    // Guarda el producto en la base de datos
+    const nuevoProducto = await producto.save();
+
+    // Asocia el producto con las categorías seleccionadas
+    if (categoriasSeleccionadas && categoriasSeleccionadas.length > 0) {
+      await Categoria.updateMany({ _id: { $in: categoriasSeleccionadas } }, { $push: { productos: nuevoProducto._id } });
+    }
+
+    res.status(201).json(nuevoProducto);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+
+
+
+// Controlador para obtener un producto por su ID
+exports.getProductoById = async (req, res) => {
+  try {
+    const producto = await Producto.findById(req.params.id);
+    if (producto == null) {
+      return res.status(404).json({ message: 'Producto no encontrado' });
+    }
+    res.json(producto);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// Controlador para obtener un producto por su ID
+exports.getProductoByCategoriaId = async (req, res) => {
+  try {
+    const categoriaId = req.params.id; // Obtenemos el ID de la categoría de la solicitud
+    const productos = await Producto.find({ categorias: categoriaId }); // Buscamos productos que tengan la categoría con el ID proporcionado
+    res.json(productos);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+
+// Controlador para actualizar un producto
+exports.updateProducto = async (req, res) => {
+  try {
+    const producto = await Producto.findById(req.params.id);
+    if (producto == null) {
+      return res.status(404).json({ message: 'Producto no encontrado' });
+    }
+    if (req.body.nombre != null) {
+      producto.nombre = req.body.nombre;
+    }
+    if (req.body.imagen != null) {
+      producto.imagen = req.body.imagen;
+    }
+    if (req.body.precio != null) {
+      producto.precio = req.body.precio;
+    }
+    if (req.body.descuento != null) {
+      producto.descuento = req.body.descuento;
+      producto.precioFinal = req.body.precio - req.body.descuento;
+    }
+    const productoActualizado = await producto.save();
+    res.json(productoActualizado);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+// Controlador para eliminar un producto
+exports.deleteProducto = async (req, res) => {
+  try {
+    const producto = await Producto.findById(req.params.id);
+    if (producto == null) {
+      return res.status(404).json({ message: 'Producto no encontrado' });
+    }
+    await Producto.deleteOne({ _id: req.params.id });
+    res.json({ message: 'Producto eliminado correctamente' });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
